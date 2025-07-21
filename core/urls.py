de99d179aa6cc6291f2b5dbb6ca8b9cf5e62dfaf
Éric Moschen <ericaugustomoschen@gmail.com>
2025-07-20 23:01:46 -0300

    Adiciona pacote holidays

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,16 @@
 
 from django.http import JsonResponse
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, redirect
 
 def health_check(request):
     return JsonResponse({"status": "ok"})
 
+def raiz(request):
+    return redirect('menuos')  # nome da sua view menuos_view
+
 urlpatterns = [
+    path('', raiz),
     path('admin/', admin.site.urls),
     path('usuario/', include('usuario.urls')),
     path('menuos/', include('menuos.urls')),
