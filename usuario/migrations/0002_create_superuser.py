4f50644b18680ebd008ae2bd7f6de15ec582af9a
Éric Moschen <ericaugustomoschen@gmail.com>
2025-07-20 23:59:41 -0300

    Criada migração para superusuário admin

--- usuario/migrations/0002_create_superuser.py
+++ usuario/migrations/0002_create_superuser.py
@@ -1,24 +1,25 @@
 from django.db import migrations
 from django.contrib.auth.hashers import make_password
 
-def create_superuser(apps, schema_editor):
+def criar_superusuario(apps, schema_editor):
     User = apps.get_model('auth', 'User')
+    
     if not User.objects.filter(username='admin').exists():
         User.objects.create(
             username='admin',
-            email='admin@email.com',
+            email='admin@example.com',
             is_staff=True,
             is_superuser=True,
             is_active=True,
-            password=make_password('admin123')  # Defina sua senha aqui
+            password=make_password('senha123')  # Troque por uma senha forte
         )
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('usuario', '0001_initial'),  # ajuste se seu último número for diferente
-    ]
+        ('auth', '0012_alter_user_first_name_max_length'),
+        ]
 
     operations = [
-        migrations.RunPython(create_superuser),
+        migrations.RunPython(criar_superusuario),
     ]
