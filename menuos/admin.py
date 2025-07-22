4fc4fd9b54b915e72685d49cf8a038412b67a6f4
EricMoschen <ericaugustomoschen@gmail.com>
2025-07-22 14:30:25 -0300

    add RegistroInicioOS

--- menuos/admin.py
+++ menuos/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import AberturaOS, Cliente, MotivoIntervencao, CentroDeCusto, Colaborador
+from .models import AberturaOS, Cliente, MotivoIntervencao, CentroDeCusto, Colaborador, RegistroInicioOS
 # Register your models here.
 admin.site.register(AberturaOS)
 admin.site.register(Cliente)
 admin.site.register(MotivoIntervencao)
 admin.site.register(CentroDeCusto)
 admin.site.register(Colaborador)
+admin.site.register(RegistroInicioOS)
